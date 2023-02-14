a5fc853a4623fd9b1b793357c897df58a7eea39b
sabrinaeskildsen <sabrina.eskildsen@kellogg.northwestern.edu>
2023-02-14 21:14:31 +0000

    class

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,11 +1,18 @@
 class CompaniesController < ApplicationController
 
   # TODO:
-  # def index
-  # end
+  def index
+    @companies = Company.all
+  end
 
   def show
     @company = Company.find_by({ "id" => params["id"] })
+  end
+
+  def new
+  end
+
+  def create
   end
 
 end
--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -1,3 +1,11 @@
 class DiceController < ApplicationController
 
+    def index
+        @die1 = rand(1..6)
+        @die2 = rand(1..6)
+        @total = @die1 + @die2
+        @username = params["username"]
+        # render the file in /view/dice/index.html.erb
+    end
+
 end
