50f40c69c5ba690af5b6e2ec007f06bd8f2be14e
sabrinaeskildsen <sabrina.eskildsen@kellogg.northwestern.edu>
2023-02-14 21:42:24 +0000

    updated

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -10,9 +10,18 @@ class CompaniesController < ApplicationController
   end
 
   def new
+    @company = Company.new
   end
 
   def create
+    @company = Company.new
+    @company["name"] = params["company"]["name"]
+    @company["city"] = params["company"]["city"]
+    @company["state"] = params["company"]["state"]
+    @company.save
+    redirect_to"/companies"
   end
+
+
 
 end
